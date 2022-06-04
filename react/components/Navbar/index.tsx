584ad6c2299fa966eb5d248852773940a7894435
Brendon Guedes <brendonguedes@icloud.com>
2022-06-04 19:02:21 -0300

    feat: criando o setup da navbar :rocket:

---
+++ react/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <h1 className="flex items-center justify-center">Navbar</h1>
+    </>
+  )
+}
+
+export default Navbar
